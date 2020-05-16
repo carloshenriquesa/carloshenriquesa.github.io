2266b63667b38c6558580b89a907d37e5f0b1a6e
Carlos Henrique <carloshenrique@MacBook-Pro-de-Carlos.local>
2020-05-15 21:51:32 -0300

    build

---
+++ precache-manifest.44287ae89fbfb8423e6c32fabf3e304a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515001fb30759909f288",
+    "url": "/css/app.24ff46d9.css"
+  },
+  {
+    "revision": "107d8912fd5a5cfd8600",
+    "url": "/css/chunk-vendors.dd750cb4.css"
+  },
+  {
+    "revision": "d648020dc8433d8736299c85c8bedb50",
+    "url": "/img/voltar.d648020d.svg"
+  },
+  {
+    "revision": "20186d5d264fd1ece01c0d6dda49542c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "515001fb30759909f288",
+    "url": "/js/app.435e9491.js"
+  },
+  {
+    "revision": "107d8912fd5a5cfd8600",
+    "url": "/js/chunk-vendors.1667204a.js"
+  },
+  {
+    "revision": "8f641cd7d57e1de699808ea766e53fed",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
